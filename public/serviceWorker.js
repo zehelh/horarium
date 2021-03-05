e81b183267129813b7984d16da83ffbc76dd6d3a
zehelh <yvan.brai@gmail.com>
2021-03-05 15:05:59 +0100

    1.1

---
+++ public/serviceWorker.js
@@ -0,0 +1,34 @@
+self.addEventListener('install', e => {
+
+  e.waitUntil(
+      caches.open('pwa')
+      .then(cache => {
+          return cache.addAll([
+              '/',
+              '/serviceWorker.js',
+              '/manifest.json',
+              '/logo.png',
+              '/index.html',
+              '/styles.css'
+          ])
+          .then(() =>self.skipWaiting())
+      })
+  )
+})
+
+self.addEventListener('activate', event => {
+
+  event.waitUntil(self.clients.claim())
+
+})
+
+self.addEventListener('fetch', event => {
+
+  event.respondWith(
+      caches.match(event.request)
+      .then(response => {
+          return response || fetch(event.request)
+      })
+  )
+
+})
